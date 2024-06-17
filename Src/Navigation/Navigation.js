9c876f1cf3e93ae366495c801c4c96320d46baa4
gauravjainn <gauravjain0761@gmail.com>
2024-06-17 11:16:10 +0530

    init project

--- Src/Navigation/Navigation.js
+++ Src/Navigation/Navigation.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../Screens/HomeScreen';
-import { createStackNavigator } from '@react-navigation/stack';
+import {CardStyleInterpolators, createStackNavigator} from '@react-navigation/stack';
+import {screenName} from './ScreenConstants';
+import IndiansPage from '../Screens/IndiansPage';
 
 const Stack = createStackNavigator();
 export default function Navigation() {
+  const headerStyleTransparent = {
+    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+    headerShown: false,
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen
+          options={({navigation}) => ({...headerStyleTransparent})}
+          name={screenName.indiansPage}
+          component={IndiansPage}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Src/Navigation/ScreenConstants.tsx
@@ -0,0 +1,4 @@
+
+export const screenName = {
+  indiansPage: 'IndiansPage'
+};
