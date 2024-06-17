5e468751368f618b456edea78377f4dea3d44987
gauravjainn <gauravjain0761@gmail.com>
2024-06-17 11:30:04 +0530

    image add

---
+++ Src/Themes/Icons.js
@@ -0,0 +1,6 @@
+export const Icons = {
+    bell: require('../assets/icons/bell.png'),
+    search: require('../assets/Images/search.png'),
+    user: require('../assets/Images/user.png'),
+    logo: require('../assets/Images/logo.png'),
+}  
