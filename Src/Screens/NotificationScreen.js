4328f68a0366daa26ba0ce80be470ac6a4ba778f
gauravjain0761 <gauravjain0761@gmail.com>
2024-06-19 16:37:32 +0530

    => Notification

--- Src/Screens/NotificationScreen.js
+++ Src/Screens/NotificationScreen.js
@@ -1,26 +1,128 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import ApplicationStyles from '../Themes/ApplicationStyles';
 import Header from '../Components/Header';
-import {FontStyle} from '../utils/commonFunction';
-import {fontname, hp, wp} from '../Themes/Fonts';
+import {FontStyle, ImageStyle} from '../utils/commonFunction';
+import {fontname, hp, screen_width, wp} from '../Themes/Fonts';
 import colors from '../Themes/Colors';
+import {Icons} from '../Themes/Icons';
+
+const Data = [
+  {
+    id: 1,
+    name: 'Rutu M',
+    content: 'has liked your post',
+    type: 'general',
+    image: Icons.userImage,
+    time: '1 hour ago',
+  },
+  {
+    id: 2,
+    name: 'Rutu M',
+    content: 'has accepted your request.',
+    type: 'general',
+    image: Icons.userImage,
+    time: '1 hour ago',
+  },
+  {
+    id: 3,
+    name: 'Sakshi Bagwari',
+    content: 'has sent you request',
+    type: 'Requests',
+    image: Icons.userImage,
+    time: '1 hour ago',
+  },
+  {
+    id: 4,
+    name: 'SOURABH CHOUGULE',
+    content: 'has accepted your request.',
+    type: 'general',
+    image: Icons.userImage,
+    time: '1 day ago',
+  },
+  {
+    id: 5,
+    name: 'Saurabh Ghode',
+    content: 'has liked your post.',
+    type: 'general',
+    image: Icons.userImage,
+    time: '3 days ago',
+  },
+  {
+    id: 6,
+    name: 'Sakshi Bagwari',
+    content: 'has liked your post.',
+    type: 'general',
+    image: Icons.userImage,
+    time: '5 days ago',
+  },
+];
 
 const NotificationScreen = () => {
+  const [categories, setCategories] = useState('All');
+
+  const renderItem = ({item, index}) => {
+    return (
+      <View style={styles.Container}>
+        <Image
+          source={item?.image}
+          style={[ImageStyle(44, 44), {borderRadius: 100, overflow: 'hidden'}]}
+          resizeMode="contain"
+        />
+        <View style={styles.nameContainer}>
+          <Text style={styles.name}>
+            {item?.name} {item?.content}
+          </Text>
+        </View>
+        <Text>{item?.time}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={ApplicationStyles.applicationView}>
       <Header logoShow={false} showLeft />
       <Text style={styles.title}>{'Notifications'}</Text>
-      <View style={styles.categoryContainer}>
-        <TouchableOpacity>
-          <Text style={styles.categoryTitle}>{'All(3)'}</Text>
+      <View style={styles.categoriesContainer}>
+        <TouchableOpacity onPress={() => setCategories('All')}>
+          <Text
+            style={[
+              styles.categoriesTitle,
+              {
+                color:
+                  categories == 'All' ? colors?.tertiary1_500 : colors?.black,
+              },
+            ]}>
+            {'All(3)'}
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.categoryTitle}>{'Requests(4)'}</Text>
+        <TouchableOpacity onPress={() => setCategories('Requests')}>
+          <Text
+            style={[
+              styles.categoriesTitle,
+              {
+                color:
+                  categories == 'Requests'
+                    ? colors?.tertiary1_500
+                    : colors?.black,
+              },
+            ]}>
+            {'Requests(4)'}
+          </Text>
         </TouchableOpacity>
       </View>
       <View style={styles.newHeader}>
         <Text style={styles.HeaderTitle}>{'New'}</Text>
+      </View>
+      <View style={styles.notificationcontainer}>
+        <FlatList data={Data} renderItem={renderItem} />
       </View>
     </View>
   );
@@ -34,11 +136,11 @@ const styles = StyleSheet.create({
     paddingLeft: wp(14),
     lineHeight: hp(26),
   },
-  categoryTitle: {
+  categoriesTitle: {
     ...FontStyle(fontname.actor_regular, 14, colors.black, '400'),
     lineHeight: hp(22),
   },
-  categoryContainer: {
+  categoriesContainer: {
     flexDirection: 'row',
     gap: wp(10),
     marginLeft: wp(17),
@@ -53,5 +155,21 @@ const styles = StyleSheet.create({
     lineHeight: hp(22),
     paddingVertical: hp(4),
     paddingLeft: wp(15),
+  },
+  notificationcontainer: {},
+  Container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  name: {
+    ...FontStyle(fontname.actor_regular, 14, colors.black, '400'),
+  },
+  nameContainer: {
+    paddingLeft: wp(15),
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+    maxWidth: wp(screen_width * 0.7),
   },
 });
